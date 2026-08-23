97dc55eabb4cc7727e859995f08c5d326f696ca9
Slicer <ahamadabdi878@gmail.com>
2026-08-23 13:48:58 +0300

    Refactor SuperSpeedCoil properties and add script
    Updated player reference and modified properties for the SuperSpeedCoil tool, including color, material, and sound settings. Added a local script for additional functionality.

--- Super Coil Remade.lua
+++ Super Coil Remade.lua
@@ -1,5 +1,4 @@
--- I cant test it because of my stupid executor (Arceus X) throwing errors in line 32
--- fuck Arceus X
+local player = game:GetService("Players")
 
 local SuperCoilR = Instance.new("Tool" )
 SuperCoilR.Name = "SuperSpeedCoil Remade"
@@ -7,13 +6,14 @@ SuperCoilR.ToolTip = "Have EVEN More Speed From The Speed Coil!"
 SuperCoilR.TextureId = "http://www.roblox.com/asset/?id=2684924567"
 SuperCoilR.Grip = CFrame.new(0, 0, 1, -1, 0, 0, 0, 1, 0, 0, 0, -1)
 SuperCoilR.RequiresHandle = true
-SuperCoilR.Parent = game.Players.LocalPlayer.Backpack
+SuperCoilR.Parent = player.LocalPlayer.Backpack
 
 local Handle = Instance.new("Part")
 Handle.Name = "Handle"
+Handle.Color = Color3.new(1, 1, 0)
+Handle.Material = Enum.Material.Neon
 Handle.Size = Vector3.new(1, 1.5570000410079956, 2.5)
 Handle.Parent = SuperCoilR
-
 
 local s = Instance.new("Sound")
 s.Name = "CoilSound"
@@ -26,13 +26,14 @@ mesh.MeshId = "rbxassetid://16606212"
 mesh.MeshType = Enum.MeshType.FileMesh
 mesh.Name = "CoilMesh"
 mesh.Scale = Vector3.new(0.699999988079071, 0.699999988079071, 0.699999988079071)
-mesh.TextureId = "rbxassetid://0"
+mesh.TextureId = "rbxassetid://156956368"
+mesh.VertexColor = Vector3.new(1, 1, 0)
 mesh.Parent = Handle
 
 local Li = Instance.new("PointLight")
 Li.Name = "CoilLight"
 Li.Brightness = 13
-Li.Color = Color3.FromRGB(255, 255, 0)
+Li.Color = Color3.new(1, 1, 0)
 Li.Range = 10
 Li.Enabled = false
 Li.Parent = Handle
@@ -47,16 +48,20 @@ UVa.Name = "UnEquipCounter"
 UVa.Value = 0
 UVa.Parent = SuperCoilR
 
+local CScript = Instance.new("LocalScript")
+CScript.Source = [[print("SUPER COIL REMAKE!!!")]]
+CScript.Parent = SuperCoilR
+
 SuperCoilR.Equipped:Connect(function()
 Va.Value += 1
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
+player.LocalPlayer.Character.Humanoid.WalkSpeed = 75
 s:Play()
 Li.Enabled = true
 end)
 
 SuperCoilR.Unequipped:Connect(function()
 UVa.Value += 1
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+player.LocalPlayer.Character.Humanoid.WalkSpeed = 16
 s:Stop()
 Li.Enabled = false
 end)
